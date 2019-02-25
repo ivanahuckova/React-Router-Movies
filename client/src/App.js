8b423451ec7fd314326c73c67ecfe94dcf9ac78b
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-25 13:49:39 +0100

    feat: Add React Route to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      savedList: []
-    };
-  }
+	constructor() {
+		super();
+		this.state = {
+			savedList: []
+		};
+	}
 
-  addToSavedList = movie => {
-    const savedList = this.state.savedList;
-    savedList.push(movie);
-    this.setState({ savedList });
-  };
+	addToSavedList = movie => {
+		const savedList = this.state.savedList;
+		savedList.push(movie);
+		this.setState({ savedList });
+	};
 
-  render() {
-    return (
-      <div>
-        <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<SavedList list={this.state.savedList} />
+				<div>Replace this Div with your Routes</div>
+				<Route path="" component={MovieList} />
+			</div>
+		);
+	}
 }
