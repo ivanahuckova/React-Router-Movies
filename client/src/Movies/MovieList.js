6d7ed3253ab3e5e64eea6fb81c1411121b48adc6
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-25 15:02:07 +0100

    feat: Create reusable MovieCard Component

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
+import MovieCard from "./MovieCard";
+
 export default class MovieList extends Component {
 	constructor(props) {
 		super(props);
@@ -37,25 +39,5 @@ where :id is the id of the individual movie.*/
 }
 
 function MovieDetails({ movie }) {
-	const { title, director, metascore, stars } = movie;
-	return (
-		<div className="movie-card">
-			<h2>
-				<Link to={`/movies/${movie.id}`}>{title}</Link>
-			</h2>
-			<div className="movie-director">
-				Director: <em>{director}</em>
-			</div>
-			<div className="movie-metascore">
-				Metascore: <strong>{metascore}</strong>
-			</div>
-			<h3>Actors</h3>
-
-			{stars.map(star => (
-				<div key={star} className="movie-star">
-					{star}
-				</div>
-			))}
-		</div>
-	);
+	return <MovieCard movie={movie} />;
 }
