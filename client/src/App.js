e10f8cff46914e5042c0f9519938247935cf22b3
Ivana Huckova <ivana.huckova@gmail.com>
2019-02-25 16:12:40 +0100

    comment: Comment out unused props

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,6 @@ export default class App extends Component {
 	// 	}));
 	// };
 	render() {
-		console.log(this.state.savedList);
 		return (
 			<div>
 				<SavedList list={this.state.savedList} />
